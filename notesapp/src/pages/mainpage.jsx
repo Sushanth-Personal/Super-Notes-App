702e1cc3bbdd1f87b528eab5b51dcdb1d9f8c5ad
Sushanth-Personal <sushanthp.calicut@gmail.com>
2024-10-03 14:01:58 +0530

    Modify: Structured the Mainpage Add: Added AddNotes Component, ChatBox Component

--- notesapp/src/pages/mainpage.jsx
+++ notesapp/src/pages/mainpage.jsx
@@ -1,19 +1,39 @@
-import styles from '../styles/mainpage.module.css'
-import AddNotes from '../components/addNotes'
-import ChatBox from '../components/ChatBox'
-const mainpage = () => {
-  return (
-    <div className = {styles.container}>
-        <div className = {styles.groupList}>
-            <div className = {styles.addNotes}>
-                <AddNotes/>
-            </div>
-        </div>
-        <div className = {styles.chatBox}>
-            <ChatBox/>
-        </div>
-        </div>
-  )
-}
+import { useState } from "react";
+import styles from "./styles/mainpage.module.css";
+import AddNotes from "../components/addNotes";
+import ChatBox from "../components/ChatBox";
+import GroupList from "../components/GroupList";
 
-export default mainpage;
+const MainPage = () => {
+  const [showAddNotes, setShowAddNotes] = useState(false);
+
+  const handleClick = () => {
+    setShowAddNotes(true);
+  };
+
+  
+  const handleOverlayClick = () => {
+    setShowAddNotes(false);
+  };
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.groupList}>
+        <GroupList />
+        <button className={styles.addNotes} onClick={handleClick}>
+          +
+        </button>
+      </div>
+      <div className={styles.chatBox}>
+        <ChatBox />
+      </div>
+      {showAddNotes && (
+        <div className={styles.overlay} onClick={handleOverlayClick}>
+          <AddNotes />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default MainPage;

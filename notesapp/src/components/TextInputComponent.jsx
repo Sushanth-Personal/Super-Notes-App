8961a544525e7ef30911d7b474c771af20fa9692
Sushanth-Personal <sushanthp.calicut@gmail.com>
2024-10-04 19:10:20 +0530

    Add: Added Current Date and time fetching functionality

--- notesapp/src/components/TextInputComponent.jsx
+++ notesapp/src/components/TextInputComponent.jsx
@@ -1,9 +1,25 @@
 import styles from "./styles/TextInputComponent.module.css";
+import formatDateAndTime from "../utils/formatDateAndTime";
+import { useState, useEffect } from "react";
 const TextInputComponent = () => {
+const [date, setDate] = useState([]);
+
+    const handleClick = () => {
+        console.log("clicked");
+        setDate(formatDateAndTime(Date.now()));
+        
+    }
+
+    useEffect(() => {
+       console.log(date);
+    }, [date]);
+
   return (
     <div className = {styles.container} >
       <textarea className = {styles.textArea} />
-      <button className = {styles.textEnterButton}>
+      <button className = {styles.textEnterButton}
+      onClick={handleClick}
+      >
         <img src='./TextEnterButton.png' />
         </button>
     </div>

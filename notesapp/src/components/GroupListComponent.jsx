c66d848f04df2e0a31063a139804d134d24d61a0
Sushanth-Personal <sushanthp.calicut@gmail.com>
2024-10-04 11:12:24 +0530

    Add: ShortForm functionality added

--- notesapp/src/components/GroupListComponent.jsx
+++ notesapp/src/components/GroupListComponent.jsx
@@ -17,10 +17,24 @@ const GroupList = ({ createdNewGroup }) => {
       <div>GroupList</div>
       {groups &&
         groups.map((group, index) => (
-          <div key={index}>{group.groupName}</div>
+          <div key={index}>
+            <div >{group.groupName}</div>
+            <div >{getShortForm(group.groupName)}</div>
+          </div>
         ))}
     </>
   );
 };
+
+function getShortForm(groupName) {
+  const words = groupName.split(' ');
+  if (words.length === 1) {
+    return words[0].charAt(0);
+  } else if (words.length === 2) {
+    return words[0].charAt(0) + words[1].charAt(0);
+  } else {
+    return words[0].charAt(0) + words[1].charAt(0);
+  }
+}
 
 export default GroupList;

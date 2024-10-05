1d953fbaddc9a486ff3d2f18edbc19023753058a
Sushanth-Personal <sushanthp.calicut@gmail.com>
2024-10-05 12:06:16 +0530

    Modify: modifed formatDateAndTime to return date or time based on request

--- notesapp/src/utils/formatDateAndTime.js
+++ notesapp/src/utils/formatDateAndTime.js
@@ -1,19 +1,17 @@
-const formatDateAndTime = (date = Date.now()) =>{
-    const newDate = new Date(date);
-    const formattedDate = newDate.toLocaleDateString("en-US", {
-        month: "long",
-        day: "numeric",
-        year: "numeric",
-    });
-    const formattedTime = newDate.toLocaleTimeString("en-US", {
-        hour: "numeric",
-        minute: "numeric",
-    });
+const formatDateAndTime = (date = Date.now(), format = "date") => {
+  const newDate = new Date(date);
+  const formattedDate = newDate.toLocaleDateString("en-US", {
+    month: "long",
+    day: "numeric",
+    year: "numeric",
+  });
+  const formattedTime = newDate.toLocaleTimeString("en-US", {
+    hour: "numeric",
+    minute: "numeric",
+  });
 
-    return {
-        date: formattedDate,
-        time: formattedTime
-    }
-}
+  if (format === "date") return formattedDate;
+  if (format === "time") return formattedTime;
+};
 
-export default formatDateAndTime
+export default formatDateAndTime;
